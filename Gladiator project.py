7d811df08e13107adbdeeb6639dde77206d16713
Muhsaa <muhammad.saad2@elev.ga.ntig.se>
2024-10-22 10:17:36 +0200

    Add files via upload

--- Gladiator project.py
+++ Gladiator project.py
@@ -1,31 +1,13 @@
 import random
 import os
 import time
-dice= [1,2,3,4,5,6,7,8,9,10]
-def punch1():
-    punch= random.choice(dice)
-    return punch,punch1
-
-def kick1():
-    kick_roll= random.choice(dice)
-    return kick1,kick_roll
-
-def slam1():
-    slam = random.choice(dice)
-    return slam
-
-def roll_dice():
-    num=random.randint(1,10)
-    return num
 
 def clear_terminal():
     if os.name=='nt':
         os.system('cls')
 
-number = roll_dice()
-punch=punch1()
-kick= kick1()
-slam= slam1()
+number = random.randint(1,10)
+number2 = random.randint(1,10)
 
 print("Hey there wake up it almost time.")
 time.sleep(2)
@@ -56,76 +38,71 @@ Fp=int(120)
 punchDamageOutput= [20,21,22,23,24,25]
 kickDamageOutput= [25,26,27,28,29,30]
 slamDamageOutput= [31,32,33,34,35,36,37,38,39,40]
-Playerattack= str(input("Punch, Slam or kick. Choose your attack: ").lower)
 lista = ["slam","punch","kick"]
-fiende = random.choice(lista)
-print("You choose: ",Playerattack)
-print(f"The enemy has chosen: {fiende}")
-if Playerattack == punch:
-    if punch >=3:
-        Fp -= random.choice(punchDamageOutput)
-
-elif Playerattack == kick:
-    if kick >=5:
-        Fp -=random.choice(kickDamageOutput)
-
-elif Playerattack == slam:
-    if slam >=7:
-        Fp -=random.choice(slamDamageOutput)
-
-if fiende == "punch":
-    print(number)
-    if number >=3:
-        Hp -= random.choice(punchDamageOutput)
-
-elif fiende == "kick":
-    print(number)
-    if number >=5:
-        Hp -= random.choice(kickDamageOutput)
-
-elif fiende == "slam":
-    print(number)
-    if number >=7:
-        Hp -=random.choice(slamDamageOutput)
-time.sleep(2)
-clear_terminal()
-print(Hp)
-print(Fp)
-while Hp > 0 or Fp > 0:
-    Playerattack = str(input("Punch, Slam or kick. Choose your attack: ").lower)
+round = 1
+battle = True
+while battle:
+    Playerattack = input("Punch, Slam or kick. Choose your attack: ")
     
     fiende = random.choice(lista)
     print(f"Fiende har valt: {fiende}")
     print("You choose: ",Playerattack)
-    if Playerattack == punch:
-        if punch >=3:
-            Fp -= random.choice(punchDamageOutput)
-
-    elif Playerattack == kick:
-        if kick >=5:
-            Fp -=random.choice(kickDamageOutput)
-
-    elif Playerattack == slam:
-        if slam >=7:
-            Fp -=random.choice(slamDamageOutput)
-
-    if fiende == "punch":
+    if Playerattack == "punch":
         print(number)
         if number >=3:
-            Hp -= random.choice(punchDamageOutput)
+            Fp -= random.choice(punchDamageOutput)
+            print("You hit him with a punch.")
+        elif number <=3:
+            print("You tried to punch him but he swiftly dodged your attack.")
 
-    elif fiende == "kick":
+    elif Playerattack == "kick":
         print(number)
         if number >=5:
-            Hp -= random.choice(kickDamageOutput)
+            Fp -=random.choice(kickDamageOutput)
+            print("You kicked him in the stomach.")
+        elif number <=5:
+            print("You tried to kick him but he blocked your attack.")
 
-    elif fiende == "slam":
+    elif Playerattack == "slam":
         print(number)
         if number >=7:
+            Fp -=random.choice(slamDamageOutput)
+            print("You grabbed him and slam him to the ground.")
+        elif number >=7:
+            print("You tried to grab him for slam but he countered you back.")
+
+    if fiende == "punch":
+        print(number2)
+        if number2 >=3:
+            Hp -= random.choice(punchDamageOutput)
+            print("the enemy punched you in the face.")
+        elif number2 <=3:
+            print("the enemy tried to punch you in the face but he missed.")
+
+    elif fiende == "kick":
+        print(number2)
+        if number2 >=5:
+            Hp -= random.choice(kickDamageOutput)
+            print("the enemy kicked you in the stomach.")
+        elif number2 <=5:
+            print("the enemy tried to kick you in the stomach bu1t he missed.")
+
+    elif fiende == "slam":
+        print(number2)
+        if number2 >=7:
             Hp -=random.choice(slamDamageOutput)
+            print("the enemy grabbed you and slam you to the ground.")
+        elif number2 >=7:
+            print("the enemy tried to grab you for slam but he missed.")
     time.sleep(2)
     clear_terminal()
     print(Hp)
     print(Fp)
-    if Fp <=0 or Hp<=0:
+    if Fp <=0:
+        print("You won the fight.Get ready for the next fight.")
+        battle = False
+        break
+    elif Hp <=0:
+        print("You lost the fight. You are dead.")
+        battle = False
         break
